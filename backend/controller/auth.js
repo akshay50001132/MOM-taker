a9c4302f9bf7dc2660e496862583917daa4da34c
akshay <akshay.shinde@ly.design>
2024-12-23 18:22:17 +0530

    signup route added

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -2,10 +2,9 @@ const User = require("../model/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const login = async (req, res) => {
+const signup = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(email, password);
 
     // hashing password to be stored in encrypted form
     const salt = await bcrypt.genSalt(10);
@@ -18,7 +17,7 @@ const login = async (req, res) => {
     });
 
     // Signing token
-    const token = jwt.sign({ id: user._id }, process.env.secret);
+    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
 
     // 30 days in milliseconds
     const THIRTY_DAYS_IN_MS = 30 * 24 * 60 * 60 * 1000;
@@ -27,13 +26,57 @@ const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         httpOnly: true,
-        secure: true,
+        secure: false,
         maxAge: THIRTY_DAYS_IN_MS,
       })
       .json(user);
   } catch (error) {
     console.log(error);
+    if (error.code == "11000") {
+      return res.status(500).json({
+        message: "This email is already registered. Please try logging in.",
+      });
+    }
     res.status(500).json({ message: "Server error, please try again later" });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(500).send({
+        message:
+          "No account found with this email. Please sign up to create a new account",
+      });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (match) {
+      // Signing token
+      const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+
+      // 30 days in milliseconds
+      const THIRTY_DAYS_IN_MS = 30 * 24 * 60 * 60 * 1000;
+
+      res
+        .status(200)
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          maxAge: THIRTY_DAYS_IN_MS,
+        })
+        .json(user);
+    } else {
+      res.status(500).send({ message: "Incorrect password. Try again." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Server error, please try again later" });
   }
 };
 
@@ -51,5 +94,6 @@ const logout = async (req, res) => {
 
 module.exports = {
   login,
+  signup,
   logout,
 };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,7 @@ const auth = require("../controller/auth");
 
 // Define the login routes
 router.post("/login", auth.login);
+router.post("/signup", auth.signup);
 router.get("/logout", auth.logout);
 
 module.exports = router;
