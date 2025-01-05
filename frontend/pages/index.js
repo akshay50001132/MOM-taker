1854300cc22d698b1c7194b9f42bd5bd727ea1f7
akshay <akshay.shinde@ly.design>
2025-01-05 19:10:18 +0530

    fetching moms

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
+import { format, parseISO } from "date-fns";
+import Link from "next/link";
 
 export default function Home() {
-  const [moms, setMoms] = useState(null);
+  const [moms, setMoms] = useState([]);
   useEffect(() => {
-    const getSelfMoms = async () => {
+    const getMoms = async () => {
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/mom/getSelfMoms`,
+          `${process.env.NEXT_PUBLIC_API_URL}/mom/getMoms`,
           {
             method: "GET",
             headers: {
@@ -18,15 +20,26 @@ export default function Home() {
         const moms = await res.json();
         if (res.status == 200) {
           setMoms(moms);
-          console.log(moms);
+          console.log(moms.length);
         }
       } catch (error) {}
     };
-    getSelfMoms();
+    getMoms();
   }, []);
   return (
     <main>
       <h1>Welcome to MOM taker.</h1>
+      {moms.length &&
+        moms.map((mom) => {
+          return (
+            <Link href={`/mom/${mom._id}`} key={mom._id}>
+              <span>
+                {format(parseISO(mom.createdAt), "d MMM yyyy, hh:mma")}
+              </span>
+              <p>{mom.title}</p>
+            </Link>
+          );
+        })}
     </main>
   );
 }
